2035394705509beaeaf46457ec7dc5bac239a9b8
Rohith.G <70756703+rohit27-2@users.noreply.github.com>
2023-06-09 12:21:16 +0530

    bmi_calci

--- Bmi_Calculator/lib/input_page.dart
+++ Bmi_Calculator/lib/input_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Icon_Content.dart';
+import 'reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const colour = Color(0xFF1D1E33);
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -12,10 +18,61 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Calculator'),
+        title: const Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text('Body Here'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  colour: colour,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: colour,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
+                  ),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              colour: colour,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: colour,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: colour,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
       ),
     );
   }
